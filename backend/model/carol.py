2071a0d436c79665db2a3cf288290d50cfd2d9d7
Carol <ss@dd>
2023-09-21 16:15:24 -0500

    metodo que le pide a la base de datos una imagen o video del producto y se la pasa al frontend

---
+++ backend/model/carol.py
@@ -0,0 +1,30 @@
+import sqlite3
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+def obtener_videos_y_fotos(producto_id):
+    conn = sqlite3.connect('mi_base_de_datos.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT video_url, foto_url FROM productos WHERE id=?", (producto_id,))
+    data = cursor.fetchone()
+
+    conn.close()
+
+    if not data:
+        return {}
+
+    videos_fotos = {
+        'video_url': data[0],
+        'foto_url': data[1]
+    }
+    return videos_fotos
+
+@app.route('/producto/<int:producto_id>/videos-fotos', methods =['GET'])
+def obtener_videos_fotos_producto(producto_id):
+    videos_fotos = obtener_videos_y_fotos(producto_id)
+    return jsonify(videos_fotos)
+
+
+
