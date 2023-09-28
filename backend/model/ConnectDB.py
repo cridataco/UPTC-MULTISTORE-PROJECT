7e3f876db2de9aeb0d45a2fda04cacd571120000
JuanDavid0 <juanochoa20029128@gmail.com>
2023-09-28 01:42:22 -0500

    Organización y optimización del CRUD. La lógica para acceder y modificar la BD se trasladó a la clase Product.py

--- backend/model/ConnectDB.py
+++ backend/model/ConnectDB.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from Product import Product, Base
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class ConnectionDB:
@@ -8,95 +10,4 @@ class ConnectionDB:
         self.engine = create_engine(db_url)
         Base.metadata.create_all(self.engine)
         self.Session = sessionmaker(bind=self.engine)
-
-    def create_Product(self, product):
-        session = self.Session()
-        try:
-            existing_Product = session.query(Product).filter_by(name=product.name,
-                                                                description=product.description).first()
-            if existing_Product:
-                return False
-            session.add(product)
-            session.commit()
-            return True
-        except Exception as e:
-            session.rollback()
-            raise e
-        finally:
-            session.close()
-
-    def update_price(self, product_id, new_price):
-        session = self.Session()
-        try:
-            product = session.query(Product).filter_by(id_=product_id).first()
-            if product:
-                product.modify_price(new_price)
-                session.commit()
-                return True
-            else:
-                return False
-        except Exception as e:
-            session.rollback()
-            raise e
-        finally:
-            session.close()
-
-    def update_discount(self, product_id, new_discount):
-        session = self.Session()
-        try:
-            product = session.query(Product).filter_by(id_=product_id).first()
-            if product:
-                product.modify_discount(new_discount)
-                session.commit()
-                return True
-            else:
-                return False
-        except Exception as e:
-            session.rollback()
-            raise e
-        finally:
-            session.close()
-
-    def update_description(self, product_id, new_description):
-        session = self.Session()
-        try:
-            product = session.query(Product).filter_by(id_=product_id).first()
-            if product:
-                product.modify_description(new_description)
-                session.commit()
-                return True
-            else:
-                return False
-        except Exception as e:
-            session.rollback()
-            raise e
-        finally:
-            session.close()
-
-    def delete_Product(self, product_id):
-        session = self.Session()
-        try:
-            product = session.query(Product).filter_by(id_=product_id).first()
-            if product:
-                session.delete(product)
-                session.commit()
-                return True
-            else:
-                return False
-        except Exception as e:
-            session.rollback()
-            raise e
-        finally:
-            session.close()
-
-    def show_Products(self):
-        session = self.Session()
-        try:
-            products = session.query(Product).all()
-            return products
-        except Exception as e:
-            session.rollback()
-            raise e
-        finally:
-            session.close()
-
+        print("Database connected successfully")
