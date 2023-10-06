127d72000f69f38fb58c196dd8d82d39e5f0a66f
JuanDavid0 <juanochoa20029128@gmail.com>
2023-10-06 17:26:44 -0500

    mensaje

--- backend/model/ConnectDB.py
+++ backend/model/ConnectDB.py
@@ -10,4 +10,7 @@ class ConnectionDB:
         self.engine = create_engine(db_url)
         Base.metadata.create_all(self.engine)
         self.Session = sessionmaker(bind=self.engine)
-        print("Database connected successfully")
+
+        #Prueba
+
+
