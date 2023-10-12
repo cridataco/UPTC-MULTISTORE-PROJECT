1ada8cd3899fda86b8e9fd874929b305e451a169
Luisen1 <luisenriquehernandezvarbuena@gmail.com>
2023-10-11 21:00:04 -0500

    Añado conexion de la clase PriceHistory con la clase Product.py

--- backend/model/Product.py
+++ backend/model/Product.py
@@ -1,10 +1,26 @@
-from sqlalchemy import Column, String, Integer, Float, DateTime, ForeignKey
+from datetime import date
+from sqlalchemy import Column, Integer, Float, Date, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.sql import func
 
 Base = declarative_base()
 
+class PriceHistory(Base):
+    __tablename__ = "price_history"
+
+    id_price_history = Column(Integer, primary_key=True, autoincrement=True)
+    start_date = Column(Date, nullable=False)
+    end_date = Column(Date, nullable=False)
+    price_actual = Column(Float, nullable=False)
+    product_id = Column(Integer, ForeignKey("products.id_product"))
+
+    product = relationship("Product", back_populates="price_history")
+
+    def display_price_history(self):
+        print(f"ID: {self.id_price_history}")
+        print(f"Start Date: {self.start_date}")
+        print(f"End Date: {self.end_date}")
+        print(f"Price Actual: {self.price_actual}")
 
 class Product(Base):
     __tablename__ = "products"
@@ -18,4 +34,13 @@ class Product(Base):
     keywords = Column(String(100), nullable=False)
     product_link = Column(String(250), nullable=False)
 
-    product_resources = relationship("ProductResource", back_populates="product")
+    price_history = relationship("PriceHistory", back_populates="product")
+
+
+if __name__ == "__main__":
+    ph = PriceHistory(start_date=date(2023, 1, 1), end_date=date(2023, 12, 31), price_actual=100.5)
+    product = Product(name_product="Example Product", reference_model="ABC123", release_date=date(2023, 1, 1), product_features="Feature 1, Feature 2", keywords="keyword1, keyword2", product_link="https://example.com")
+
+    product.price_history.append(ph)
+
+    ph.display_price_history()
