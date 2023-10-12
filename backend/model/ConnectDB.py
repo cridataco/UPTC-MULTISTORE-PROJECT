c398e03f74a7f7f5095a0b9d009850b56fc45a14
JuanDavid0 <juanochoa20029128@gmail.com>
2023-10-11 19:26:32 -0500

    .

--- backend/model/ConnectDB.py
+++ backend/model/ConnectDB.py
@@ -10,7 +10,3 @@ class ConnectionDB:
         self.engine = create_engine(db_url)
         Base.metadata.create_all(self.engine)
         self.Session = sessionmaker(bind=self.engine)
-
-        #Prueba
-
-
