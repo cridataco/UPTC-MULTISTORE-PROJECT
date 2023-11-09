8215e9ac3b8d257aa8f527d5dce87574d0466d57
Thomas Sorza <76008678+ThomasSorza@users.noreply.github.com>
2023-11-09 08:14:29 -0500

    added triggers

--- database/triggers/triggers.py
+++ database/triggers/triggers.py
@@ -1,1 +1,46 @@
-#Script to create db_triggers
+import mysql.connector
+
+# Conectar a la base de datos
+conn = mysql.connector.connect( #coneccion a la base de datos
+    host="tu_host",
+    user="tu_usuario", 
+    password="tu_contraseña",
+    database="tu_base_de_datos"
+)
+
+# Crear un cursor
+cursor = conn.cursor()
+
+# Crear el procedimiento almacenado
+create_procedure_query = """
+DELIMITER //
+
+CREATE PROCEDURE GenerateDispatchReport()
+BEGIN
+    SELECT *
+    FROM orders
+    WHERE state = 'Pending' -- no enviado aun
+    AND DATE(delivery_date) = CURDATE(); 
+END //
+
+DELIMITER ;
+"""
+#la funcion CURDATE() devuelve la fecha actual del en el formato 'YYYY-MM-DD'. tambien se puede usar SYSdate.
+
+cursor.execute(create_procedure_query)
+
+# Crear el evento (trigger)
+create_pedidos_query = """
+CREATE EVENT IF NOT EXISTS DailyDispatchReport
+ON SCHEDULE EVERY 1 DAY
+DO
+BEGIN
+    CALL GenerateDispatchReport();
+END;
+"""
+
+cursor.execute(create_pedidos_query)
+
+# Confirmar los cambios y cerrar la conexión
+conn.commit()
+conn.close()
